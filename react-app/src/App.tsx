c2b630ed800ecd9635de094296e56086ff19584f
Sofia <dev@smcf.io>
2021-03-23 01:46:55 -0400

    Add chart

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, {useState} from "react";
 import {useForm} from 'react-hook-form';
 import {doFetch} from "./ApiClient";
 import {Form} from "./components/Form";
+import {Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts'
 
 const App = () => {
     const {register, handleSubmit} = useForm();
+    const [result, setResult] = useState<number[][]>([]);
 
-    const onSubmit = async (data: any) => await doFetch('/corrode', {
+    const onSubmit = async (data: any) => setResult(await doFetch('/corrode', {
         corner_diff_boost: 0.003,
         circle_diff_boost: 0.8,
         crack_rate: 0.05,
         crack_diff: 0.002,
         ...data
-    })
+    }))
+
+    const data = React.useMemo(() =>
+            (result[0] || []).map((el: number, i: number,) => ({name: result[1][i], amt: el})),
+        [result]
+    );
 
     return <main>
         <header>
             <h1>Bridge Corrosion Simulator</h1>
-            <h2>Parameters</h2>
-            <Form onSubmit={handleSubmit(onSubmit)} register={register}/>
         </header>
+        <h2>Parameters</h2>
+        <Form onSubmit={handleSubmit(onSubmit)} register={register}/>
+        <br/>
+        {result && <data>
+            <LineChart width={500} height={250} data={data}
+                       margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <XAxis dataKey="name"/>
+                <Line type="monotone" dataKey={"amt"} stroke="#8884d8"/>
+                <YAxis/>
+                <Tooltip/>
+            </LineChart>
+        </data>}
     </main>;
 };
 
